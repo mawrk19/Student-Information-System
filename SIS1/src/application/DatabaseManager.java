26e9a27ce9352f41d8c1a6b05a4879f3cfd223d7
Mark Acedo <gerceeacedo@gmail.com>
2023-10-15 13:56:10 +0800

    User Instance and DAO

---
+++ SIS1/src/application/DatabaseManager.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+	
+	private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/sis_database";
+	private static final String DB_USERNAME = "root";
+	private static final String DB_PASSWORD = "";
+	
+	public static Connection getConnection() {
+		
+		try {
+			return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
--- SIS1/src/application/Main.java
+++ SIS1/src/application/Main.java
@@ -2,7 +2,7 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+	import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
