d79c16ded4daca3bb7c1c3253c3256a260010044
Mark Acedo <gerceeacedo@gmail.com>
2023-10-16 18:41:50 +0800

    deleter

--- SIS1/src/application/Main.java
+++ SIS1/src/application/Main.java
@@ -13,7 +13,10 @@ public class Main extends Application {
         // Load the FXML file
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainFrame.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 1200, 900);
+//        //scene.getStylesheets().add(getClass().getResource("mainframe.css").toExternalForm());s      
+//        String mfcss = this.getClass().getResource("mainframe.css").toExternalForm();
+//        scene.getStylesheets().add(mfcss);
         primaryStage.setTitle("Student Information System");
         primaryStage.setScene(scene);
         primaryStage.show();
