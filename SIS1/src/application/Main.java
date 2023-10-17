34eff193abdca8e238db04474e627dff85164d39
Mark Acedo <gerceeacedo@gmail.com>
2023-10-17 14:18:36 +0800

    new login form

--- SIS1/src/application/Main.java
+++ SIS1/src/application/Main.java
@@ -2,24 +2,28 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-	import javafx.scene.Parent;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
         // Load the FXML file
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainFrame.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root, 1200, 900);
-//        //scene.getStylesheets().add(getClass().getResource("mainframe.css").toExternalForm());s      
-//        String mfcss = this.getClass().getResource("mainframe.css").toExternalForm();
-//        scene.getStylesheets().add(mfcss);
-        primaryStage.setTitle("Student Information System");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    	Parent root = FXMLLoader.load(getClass().getResource("/com/login/MainLogin.fxml"));        
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.show();
+        
+//      //scene.getStylesheets().add(getClass().getResource("mainframe.css").toExternalForm());s      
+//      String mfcss = this.getClass().getResource("mainframe.css").toExternalForm();
+//      scene.getStylesheets().add(mfcss);
     }
 
     public static void main(String[] args) {
