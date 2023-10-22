8ed14ce2c7662e88b9708c8eefdbd9d134d7b328
Mawrk19 <gerceeacedo@gmail.com>
2023-10-22 09:34:59 +0800

    temporary palette

--- SIS1/src/application/Main.java
+++ SIS1/src/application/Main.java
@@ -29,5 +29,22 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
     }
 }
