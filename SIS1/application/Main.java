652178b107cf749e2a81c92ac114a61c0bf45572
Mark Acedo <gerceeacedo@gmail.com>
2023-10-25 23:57:49 +0800

    added registration form

--- SIS1/application/Main.java
+++ SIS1/application/Main.java
@@ -13,15 +13,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Load the FXML file
-    	//Parent root = FXMLLoader.load(getClass().getResource("/com/login/MainLogin.fxml"));  
-    	Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("/com/login/MainLogin.fxml"));  
+    	//Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
-        
 //      //scene.getStylesheets().add(getClass().getResource("mainframe.css").toExternalForm());s      
 //      String mfcss = this.getClass().getResource("mainframe.css").toExternalForm();
 //      scene.getStylesheets().add(mfcss);
