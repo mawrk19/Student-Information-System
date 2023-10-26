b834ff0b7f682a878b0f8d69b59c07fd87a33dd7
Ezedrex <ezzbenig24@gmail.com>
2023-10-26 17:25:33 +0800

    ano toh?

--- SIS1/application/Main.java
+++ SIS1/application/Main.java
@@ -13,11 +13,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-    	//Parent root = FXMLLoader.load(getClass().getResource("/com/login/MainLogin.fxml"));  
-    	Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("/com/login/MainLogin.fxml"));  
+    	//Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
-        stage.setMaximized(true);
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
