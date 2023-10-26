5587fce0bab2ad91f02b43b2d2dfbedcb7a18eff
Ezedrex <ezzbenig24@gmail.com>
2023-10-26 18:31:57 +0800

    fixing student profile

--- SIS1/application/MainFrameController.java
+++ SIS1/application/MainFrameController.java
@@ -1,32 +1,43 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 
 public class MainFrameController implements Initializable{
 
     @FXML
-    private Label StudentProf;
+    private Button StudentProf;
+    
+    @FXML 
+    private StackPane stackarea;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		StudentProf.setOnMouseClicked(this::StudentProfScene);
 	}
 	
 	@FXML
-    void StudentProfScene(MouseEvent event) {
+	public void StudentProfbtn(ActionEvent event) throws IOException {
 		 StudentProf.setStyle("-fx-background-color: #eff0f3;-fx-border-radius: 25 0 0 25; -fx-background-radius: 25 0 0 25;");
 		 StudentProf.setTextFill(Color.BLACK);
+		 
+			 //Parent fxml = FXMLLoader.load(getClass().getResource("/studentprof/StudentProfile.fxml"));
+				//stackarea.getChildren().removeAll();
+				//stackarea.getChildren().setAll(fxml);
+		 
     }
-    
-    
     
 
 }
