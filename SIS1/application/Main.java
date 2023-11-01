f657a1ac30055ac5e83e90140e4991a595242ada
Mark Acedo <gerceeacedo@gmail.com>
2023-11-01 14:58:20 +0800

    fixed logi

--- SIS1/application/Main.java
+++ SIS1/application/Main.java
@@ -13,11 +13,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-    	//Parent root = FXMLLoader.load(getClass().getResource("/com/login/MainLogin.fxml"));  
-    	Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
-        Scene scene = new Scene(root, 1200, 850);
+    	Parent root = FXMLLoader.load(getClass().getResource("/com/login/MainLogin.fxml"));  
+    	//Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
+        Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
-        stage.setFullScreen(true);
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
