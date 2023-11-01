74cbc5654fb2ae42edce72ef44b1d671a0225a3f
leslie mae aya-ay <leslie mae aya-ay@LAPTOP-ARMTLHC4>
2023-11-02 03:10:10 +0800

    eto na talaga layout

--- SIS1/schedule/ScheduleController.java
+++ SIS1/schedule/ScheduleController.java
@@ -1,5 +1,18 @@
 package schedule;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
 public class ScheduleController {
 
 }
