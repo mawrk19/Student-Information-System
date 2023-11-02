c387699be9c1cb72b12e1ff975b56f7874cc38b8
leslie mae aya-ay <leslie mae aya-ay@LAPTOP-ARMTLHC4>
2023-11-02 21:52:15 +0800

    patry nga po kung responsive

--- SIS1/schedule/ScheduleController.java
+++ SIS1/schedule/ScheduleController.java
@@ -3,7 +3,6 @@ package schedule;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeTableColumn;
