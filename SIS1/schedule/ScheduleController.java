b85aaeebc1617571f8ce4cf4d7a5a268f182fc8c
leslie mae aya-ay <leslie mae aya-ay@LAPTOP-ARMTLHC4>
2023-11-07 23:37:59 +0800

    papush

--- SIS1/schedule/ScheduleController.java
+++ SIS1/schedule/ScheduleController.java
@@ -1,17 +1,52 @@
 package schedule;
 
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class ScheduleController {
+import java.io.IOException;
 
-}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+	   public class ScheduleController {
+	       @FXML
+	       private Button BtnTimetable;
+
+	       public void initialize() {
+	           BtnTimetable.setOnMouseClicked(event -> {
+	               
+	                FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/MainFrame.fxml"));
+	                Parent root = null;
+					try {
+						root = loader.load();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	                Stage stage = (Stage) BtnTimetable.getScene().getWindow();
+	                Scene scene = new Scene(root);
+	                stage.setScene(scene);
+	           });
+	       
+	       }
+	   }
