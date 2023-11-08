7bd1485caa76999ef4701b6de31ced50fee66d2b
Mark Acedo <gerceeacedo@gmail.com>
2023-11-08 11:50:08 +0800

    enrollment stud function donne

--- SIS1/enrollment/EnrollmentController.java
+++ SIS1/enrollment/EnrollmentController.java
@@ -1,75 +1,138 @@
 package enrollment;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import application.DatabaseManager;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
-public class EnrollmentController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
-//	@FXML
-//	TextField fname, mname, lname, sid;
-//	@FXML
-//	PasswordField passField;
-//	@FXML
-//	Button signInBTN;
-//	@FXML
-//	Label messLabel, tbox;
-//	@FXML
-//	ComboBox<String> yrLvl, section, course, gender, location;
-//
-//	public void exportdata(ActionEvent e) {
-//		DatabaseManager connectNow = new DatabaseManager();
-//		Connection con = connectNow.getConnection();
-//
-//		String fname1 = fname.getText();
-//		String mname1 = mname.getText();
-//		String lname1 = lname.getText();
-//		String sid1 = sid.getText();
-//		String yr = yrLvl.getValue();
-//		String sectionValue = section.getValue();
-//		String courseValue = course.getValue();
-//		String genderValue = gender.getValue();
-//		String locationValue = location.getValue();
-//
-//		if (fname1.isEmpty() || mname1.isEmpty() || lname1.isEmpty() || sid1.isEmpty() || yr == null
-//				|| sectionValue == null || courseValue == null || genderValue == null || locationValue == null) {
-//			tbox.setText("Incomplete information");
-//			return;
-//		}
-//
-//		String infos = "INSERT INTO students (First_name, Middle_name, last_name, SID, course, Year, gender, location, section) VALUES (?,?,?,?,?,?,?,?,?)";
-//
-//		try {
-//			PreparedStatement stmt = con.prepareStatement(infos);
-//
-//			stmt.setString(1, fname1);
-//			stmt.setString(2, mname1);
-//			stmt.setString(3, lname1);
-//			stmt.setString(4, sid1);
-//			stmt.setString(5, courseValue);
-//			stmt.setString(6, yr);
-//			stmt.setString(7, genderValue);
-//			stmt.setString(8, locationValue);
-//			stmt.setString(9, sectionValue);
-//
-//			int rs = stmt.executeUpdate();
-//			if (rs == 1) {
-//				tbox.setText("Successfully Enrolled");
-//			} else {
-//				tbox.setText("Failed Registration");
-//			}
-//		} catch (SQLException ex) {
-//			ex.printStackTrace();
-//			tbox.setText("Error: " + ex.getMessage());
-//		}
-//	}
+import application.DatabaseManager;
+
+public class EnrollmentController implements Initializable {
+
+    @FXML
+    private ComboBox<String> courseCMB;
+
+    @FXML
+    private TextField dateTF;
+
+    @FXML
+    private Button enrollBTN;
+
+    @FXML
+    private TextField fNameTF;
+
+    @FXML
+    private ComboBox<String> genderCMB;
+
+    @FXML
+    private ImageView insertIMG;
+
+    @FXML
+    private TextField lNameTF;
+
+    @FXML
+    private ComboBox<String> locCMB;
+
+    @FXML
+    private TextField mNameTF;
+
+    @FXML
+    private ComboBox<String> secCMB;
+
+    @FXML
+    private TextField sidTF;
+
+    @FXML
+    private ComboBox<String> yrCMB;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Initialize ComboBoxes with sample data
+        ObservableList<String> courses = FXCollections.observableArrayList("BSCS", "BSIT", "BSIS", "BSEMC");
+        courseCMB.setItems(courses);
+
+        ObservableList<String> genders = FXCollections.observableArrayList("Male", "Female");
+        genderCMB.setItems(genders);
+
+        ObservableList<String> locations = FXCollections.observableArrayList("Camarin", "Congress", "South");
+        locCMB.setItems(locations);
+
+        ObservableList<String> sections = FXCollections.observableArrayList("A", "B", "C");
+        secCMB.setItems(sections);
+
+        ObservableList<String> years = FXCollections.observableArrayList("1st", "2nd", "3rd","4th");
+        yrCMB.setItems(years);
+    }
+
+    @FXML
+    private void enrollButtonClicked() {
+        String selectedCourse = courseCMB.getValue();
+        String enrollmentDate = dateTF.getText();
+        String firstName = fNameTF.getText();
+        String gender = genderCMB.getValue();
+        String location = locCMB.getValue();
+        String lastName = lNameTF.getText();
+        String middleName = mNameTF.getText();
+        String section = secCMB.getValue();
+        
+        String year = "2023"; // Define the year value here or get it from a ComboBox
+
+        try {
+            Connection con = DatabaseManager.getConnection();
+            String sql = "INSERT INTO students (course, date, First_name, gender, location, last_name, Middle_name, section, year) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, selectedCourse);
+            preparedStatement.setString(2, enrollmentDate);
+            preparedStatement.setString(3, firstName);
+            preparedStatement.setString(4, gender);
+            preparedStatement.setString(5, location);
+            preparedStatement.setString(6, lastName);
+            preparedStatement.setString(7, middleName);
+            preparedStatement.setString(8, section);
+            preparedStatement.setString(9, year);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+
+            if (rowsInserted > 0) {
+                // Retrieve the auto-generated ID
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int generatedId = generatedKeys.getInt(1);
+                    String formattedId = String.format("%s%04d", year, generatedId);
+                    sidTF.setText(formattedId);
+                }
+
+                System.out.println("Enrollment successful!");
+                
+                courseCMB.setValue(null);
+                dateTF.clear();
+                fNameTF.clear();
+                genderCMB.setValue(null);
+                locCMB.setValue(null);
+                lNameTF.clear();
+                mNameTF.clear();
+                secCMB.setValue(null);
+            }
+
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle database errors here
+        }
+    }
 
 }
