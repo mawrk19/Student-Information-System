1911b49fe21b858d2ac057eb5a447d6b00f6dfd2
Ezedrex <ezzbenig24@gmail.com>
2023-11-28 12:17:40 +0800

    added calendar sa sched

--- SIS1/schedule/ScheduleController.java
+++ SIS1/schedule/ScheduleController.java
@@ -2,51 +2,100 @@ package schedule;
 
 
 
-import javafx.geometry.Insets;
-import javafx.scene.Cursor;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeTableColumn;
-import javafx.scene.control.TreeTableView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.ZonedDateTime;
+import java.util.*;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+public class ScheduleController implements Initializable{
 
-	   public class ScheduleController {
-	       @FXML
-	       private Button BtnTimetable;
+		ZonedDateTime dateFocus;
+	    ZonedDateTime today;
+	    
+	    @FXML
+	    private Text month;
+	    
+	    @FXML
+	    private Text year;
+	    
+	    @FXML
+	    private FlowPane calendar;
+		   
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			// TODO Auto-generated method stub
+			dateFocus = ZonedDateTime.now();
+	        today = ZonedDateTime.now();
+	        
+	        drawCalendar();
+		}
+		
+		private void drawCalendar(){
+	        year.setText(String.valueOf(dateFocus.getYear()));
+	        month.setText(String.valueOf(dateFocus.getMonth()));
 
-	       public void initialize() {
-	           BtnTimetable.setOnMouseClicked(event -> {
-	               
-	                FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/MainFrame.fxml"));
-	                Parent root = null;
-					try {
-						root = loader.load();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-	                Stage stage = (Stage) BtnTimetable.getScene().getWindow();
-	                Scene scene = new Scene(root);
-	                stage.setScene(scene);
-	           });
+	        double calendarWidth = calendar.getPrefWidth();
+	        double calendarHeight = calendar.getPrefHeight();
+	        double strokeWidth = 1;
+	        double spacingH = calendar.getHgap();
+	        double spacingV = calendar.getVgap();
+
+	        int monthMaxDate = dateFocus.getMonth().maxLength();
+	        //Check for leap year
+	        if(dateFocus.getYear() % 4 != 0 && monthMaxDate == 29){
+	            monthMaxDate = 28;
+	        }
+	        int dateOffset = ZonedDateTime.of(dateFocus.getYear(), dateFocus.getMonthValue(), 1,0,0,0,0,dateFocus.getZone()).getDayOfWeek().getValue();
+
+	        for (int i = 0; i < 6; i++) {
+	            for (int j = 0; j < 7; j++) {
+	                StackPane stackPane = new StackPane();
+
+	                Rectangle rectangle = new Rectangle();
+	                rectangle.setFill(Color.TRANSPARENT);
+	                rectangle.setStroke(Color.BLACK);
+	                rectangle.setStrokeWidth(strokeWidth);
+	                double rectangleWidth =(calendarWidth/7) - strokeWidth - spacingH;
+	                rectangle.setWidth(rectangleWidth);
+	                double rectangleHeight = (calendarHeight/6) - strokeWidth - spacingV;
+	                rectangle.setHeight(rectangleHeight);
+	                stackPane.getChildren().add(rectangle);
+	                
+	                
+
+	                int calculatedDate = (j+1)+(7*i);
+	                if(calculatedDate > dateOffset){
+	                    int currentDate = calculatedDate - dateOffset;
+	                    if(currentDate <= monthMaxDate){
+	                        Text date = new Text(String.valueOf(currentDate));
+	                        double textTranslationY = - (rectangleHeight / 2) * 0.75;
+	                        date.setTranslateY(textTranslationY);
+	                        stackPane.getChildren().add(date);
+	                    }
+	                    if(today.getYear() == dateFocus.getYear() && today.getMonth() == dateFocus.getMonth() && today.getDayOfMonth() == currentDate){
+	                        rectangle.setStroke(Color.BLUE);
+	                    }
+	                }
+	                calendar.getChildren().add(stackPane);
+	            }
+	        }
+	    }
+
 	       
-	       }
 	   }
