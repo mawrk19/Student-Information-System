306591a67f0202d9c6d7eebf8e1005bec0b761e7
Ezedrex <ezzbenig24@gmail.com>
2023-12-03 01:41:06 +0800

    ito na ule

--- SIS1/application/Main.java
+++ SIS1/application/Main.java
@@ -14,7 +14,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
     	Parent root = FXMLLoader.load(getClass().getResource("/com/login/MainLogin.fxml"));  
-   	//	Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
+//   		Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
+    	
+//    	Parent root = FXMLLoader.load(getClass().getResource("/encoderui/Encoder.fxml"));
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
