6f85cded47fd98b78616e145f6b39c7b04f9b805
Mark Acedo <gerceeacedo@gmail.com>
2023-12-04 09:33:50 +0800

    initial transac DAO

---
+++ SIS1/enrollment/Transaction.java
@@ -0,0 +1,15 @@
+package enrollment;
+
+public class Transaction {
+	
+	private int transacID;
+	private String MOP;
+	private int amountDue;
+	private int amount;
+	private String late;
+	private int total;
+	private int balance;
+	private int miscTotal;
+	private int tuitionTotal;
+	private String encoder;
+}
