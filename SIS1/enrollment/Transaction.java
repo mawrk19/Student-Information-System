d5e81e91c9a7eedb249a3a8f5fb9ee628fff536e
Mark Acedo <gerceeacedo@gmail.com>
2023-12-04 22:24:38 +0800

    initial calculation

--- SIS1/enrollment/Transaction.java
+++ SIS1/enrollment/Transaction.java
@@ -1,5 +1,14 @@
 package enrollment;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import application.DatabaseManager;
+import javafx.fxml.FXML;
+import students.Students;
+
 public class Transaction {
 	
 	private int transacID;
@@ -12,4 +21,113 @@ public class Transaction {
 	private int miscTotal;
 	private int tuitionTotal;
 	private String encoder;
+
+	// Constructors, if needed
+
+	public int getTransacID() {
+		return transacID;
+	}
+
+	public void setTransacID(int transacID) {
+		this.transacID = transacID;
+	}
+
+	public String getMOP() {
+		return MOP;
+	}
+
+	public void setMOP(String MOP) {
+		this.MOP = MOP;
+	}
+
+
+	@FXML void setTransaction() {
+		try (Connection con = DatabaseManager.getConnection();
+				PreparedStatement stmt = con.prepareStatement("SELECT * FROM transaction")) {
+
+			try (ResultSet resultSet = stmt.executeQuery()) {
+				while (resultSet.next()) {
+
+					// Retrieve values from the result set and set them using setters
+					setTransacID(resultSet.getInt("transaction_ID"));
+					setMOP(resultSet.getString("payment_mode"));
+					setAmountDue(resultSet.getInt("amt_due"));
+					setAmount(resultSet.getInt("amount"));
+					setLate(resultSet.getString("late"));
+					setTotal(resultSet.getInt("total"));
+					setBalance(resultSet.getInt("balance"));
+					setMiscTotal(resultSet.getInt("misc_total"));
+					setTuitionTotal(resultSet.getInt("tuition_total"));
+					setEncoder(resultSet.getString("encoder"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace(); // Handle the exception as needed
+		}
+	}
+
+	// Getter and Setter for amountDue
+	public int getAmountDue() {
+		return amountDue;
+	}
+
+	public void setAmountDue(int amountDue) {
+		this.amountDue = amountDue;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getLate() {
+		return late;
+	}
+	
+	public void setLate(String late) {
+		this.late = late;
+	}
+	
+	public int getTotal() {
+		return total;
+	}
+	
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public int getMiscTotal() {
+		return miscTotal;
+	}
+	
+	public void setMiscTotal(int miscTotal) {
+		this.miscTotal = miscTotal;
+	}
+	
+	public int getTuitionTotal() {
+		return tuitionTotal;
+	}
+	
+	public void setTuitionTotal(int tuitionTotal) {
+		this.tuitionTotal = tuitionTotal;
+	}
+
+	public String getEncoder() {
+		return encoder;
+	}
+
+	public void setEncoder(String encoder) {
+		this.encoder = encoder;
+	}
 }
--- SIS1/enrollment/TransactionController.java
+++ SIS1/enrollment/TransactionController.java
@@ -1,13 +1,14 @@
 package enrollment;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 import application.DatabaseManager;
@@ -17,25 +18,28 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import students.Students;
 
 public class TransactionController {
 
     @FXML
     private ComboBox<String> MOPCMB;
+    
+    @FXML
+    private ComboBox<String> schemeCMB;
 
     @FXML
-    private TextField amtDueTF;
+    private Label amtDueLBL;
 
     @FXML
-    private TextField amtTF;
+    private Label amtLBL;
 
     @FXML
     private CheckBox athCB;
 
     @FXML
-    private TextField balanceTF;
+    private Label balanceLBL;
 
     @FXML
     private CheckBox comCB;
@@ -53,7 +57,7 @@ public class TransactionController {
     private CheckBox mediaCB;
 
     @FXML
-    private TextField miscTF;
+    private Label miscLBL;
 
     @FXML
     private CheckBox sciCB;
@@ -65,77 +69,147 @@ public class TransactionController {
     private TextField transacIDTF;
 
     @FXML
-    private TextField tuitionTF;
-    
-    private int tuiton = 1000;
+    private Label tuitionLBL;
 
-  
-	public void initialize(URL url, ResourceBundle rb) {
-    	ObservableList<String> mop = FXCollections.observableArrayList("Scholar", "Full");
-		MOPCMB.setItems(mop);
-		
-		ObservableList<String> late = FXCollections.observableArrayList("Yes", "No");
-		lateCMB.setItems(late);
-		
+    private int tuition = 1000;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<String> mop = FXCollections.observableArrayList("Scholar", "Full");
+        MOPCMB.setItems(mop);
+
+        ObservableList<String> late = FXCollections.observableArrayList("Yes", "No");
+        lateCMB.setItems(late);
+        
+        ObservableList<String> scheme = FXCollections.observableArrayList("1", "2");
+        schemeCMB.setItems(scheme);
     }
-	
-	private void saveAndPrint() {
-		
-	}
+
+
+    private void print(Transaction transac) {
     
-	private void saveAndPrintClicked( Transaction transac) throws SQLException, IOException {
-		String mop = MOPCMB.getValue();
-		String transacID = transacIDTF.getText();
-		String amtDue = amtDueTF.getText(); // make label
-		String amt = amtTF.getText();
-		String late = lateCMB.getValue();
-		String total = totalTF.getText(); // make label
-		String balance = balanceTF.getText(); // make label
-		//get all the checkbox value
-		//String miscTotal = yrCMB.getValue(); // make label
-		String tuition = tuitionTF.getText(); // make label
-		
-		if (mop == null || transacID.isEmpty() || amt.isEmpty() || late == null) {
-	        System.out.println("Please fill in all required fields.");
-	        return;
-	    }
+    }
 
-		UserSession session = UserSession.getInstance();
-		String username = session.getUsername();
+    private void saveAndPrintClicked(Transaction transac) {
+    	String mop = MOPCMB.getValue();
+        String transacID = transacIDTF.getText();
+        String late = lateCMB.getValue();
+        String scheme = schemeCMB.getValue();
 
-			try (Connection con = DatabaseManager.getConnection()) {
-				String sql;
+        if (mop == null || transacID.isEmpty() || late == null || scheme == null) {
+            System.out.println("Please fill in all required fields.");
+            return;
+        }
 
-			
-					sql = "INSERT INTO student (transaction_ID, payment_mode, amt_due, amount, late, total, balance, misc_total, tuition_total, encoder, date) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        double tuitionTotal;
+        double miscTotal = calculateMiscAmount();
 
-				try (PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-					preparedStatement.setString(1, transacID);
-					preparedStatement.setString(2, mop);
-					preparedStatement.setString(3, amtDue);
-					preparedStatement.setString(4, amt);
-					preparedStatement.setString(5, late);
-					preparedStatement.setString(6, total);
-					preparedStatement.setString(7, balance);
-					preparedStatement.setString(8, miscTotal);
-					preparedStatement.setString(9, tuition);
-					preparedStatement.setString(10, encoder);
-					preparedStatement.setString(10, date);
-					
-					int rowsAffected = preparedStatement.executeUpdate();
+        // Set tuitionTotal based on the selected scheme
+        if ("1".equals(scheme)) {
+            tuitionTotal = 1000.0;
+        } else if ("2".equals(scheme)) {
+            tuitionTotal = 3500.0;
+        } else {
+            System.out.println("Invalid scheme selected.");
+            return;
+        }
 
-					if (rowsAffected > 0) {
-						try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
-							if (generatedKeys.next()) {
-								// blah blah
-							}
-						}
-					} else {
-						System.out.println("No rows affected. Insertion failed.");
-					}
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} 
+        // Adjust tuitionTotal based on the payment mode (mop)
+        if ("Scholar".equals(mop)) {
+            tuitionTotal -= 1000.0;
+        }
+
+        // Set labels and text fields
+        tuitionLBL.setText(String.valueOf(tuitionTotal));
+
+        // Set total amount
+        double totalAmount = tuitionTotal + miscTotal;
+        totalTF.setText(String.valueOf(totalAmount));
+
+        // Adjust balance based on late enrollment
+        double balanceTotal = totalAmount;
+        if ("Yes".equals(late)) {
+            balanceTotal += 50.0;
+        }
+
+        // Set labels for total and balance
+        amtLBL.setText(String.valueOf(totalAmount));
+        balanceLBL.setText(String.valueOf(balanceTotal));
+
+        UserSession session = UserSession.getInstance();
+        String encoder = session.getUsername();
+
+        try (Connection con = DatabaseManager.getConnection()) {
+            String sql = "INSERT INTO transaction (transaction_ID, payment_mode, amt_due, amount, late, total, balance, misc_total, tuition_total, encoder, date, scheme) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+
+            try (PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, transacID);
+                preparedStatement.setString(2, mop);
+                preparedStatement.setString(3, amtDueLBL.getText());
+                preparedStatement.setString(4, amtLBL.getText());
+                preparedStatement.setString(5, late);
+                preparedStatement.setString(6, totalTF.getText());
+                preparedStatement.setString(7, balanceLBL.getText());
+                preparedStatement.setDouble(8, miscTotal); // Use setDouble for numeric values
+                preparedStatement.setString(9, tuitionLBL.getText());
+                preparedStatement.setString(10, encoder);
+                preparedStatement.setString(11, getCurrentDate());
+                preparedStatement.setString(12, scheme); // Assuming scheme is a string in your database
+
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                        if (generatedKeys.next()) {
+                            // Do something with the generated keys
+                        }
+                    }
+                } else {
+                    System.out.println("No rows affected. Insertion failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private double calculateMiscAmount() {
+        double miscTotal = 0.0;
+
+        // Define fees for each checkbox
+        double libFee = 30.0;
+        double medFee = 40.0;
+        double sciFee = 100.0;
+        double comFee = 100.0;
+        double athFee = 30.0;
+        double mediaFee = 40.0;
+
+        // Check which checkboxes are selected and accumulate the fees
+        if (libCB.isSelected()) {
+            miscTotal += libFee;
+        }
+        if (medCB.isSelected()) {
+            miscTotal += medFee;
+        }
+        if (sciCB.isSelected()) {
+            miscTotal += sciFee;
+        }
+        if (comCB.isSelected()) {
+            miscTotal += comFee;
+        }
+        if (athCB.isSelected()) {
+            miscTotal += athFee;
+        }
+        if (mediaCB.isSelected()) {
+            miscTotal += mediaFee;
+        }
+
+        return miscTotal;
+    }
+
+    private String getCurrentDate() {
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return currentDate.format(formatter);
+    }
 }
