25c8dd3038bcf441365ca3e198b257cc3aee2fb6
Mark Acedo <gerceeacedo@gmail.com>
2023-12-06 15:53:28 +0800

    status add

--- SIS1/enrollment/TransactionController.java
+++ SIS1/enrollment/TransactionController.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import application.DatabaseManager;
@@ -78,8 +79,6 @@ public class TransactionController {
     @FXML
     private Label totalLBL;
 
-    @FXML
-    private TextField transacIDTF;
 
     @FXML
     private TextField amtTF;
@@ -114,7 +113,6 @@ public class TransactionController {
         MOPCMB.setOnAction(event -> setTransactionBasedOnSelection());
         schemeCMB.setOnAction(event -> setTransactionBasedOnSelection());
         lateCMB.setOnAction(event -> setTransactionBasedOnSelection());
-        transacIDTF.setOnAction(event -> setTransactionBasedOnSelection());
         amtTF.setOnAction(event -> setTransactionBasedOnSelection());
         libCB.setOnAction(event -> setTransactionBasedOnSelection());
         medCB.setOnAction(event -> setTransactionBasedOnSelection());
@@ -159,7 +157,6 @@ public class TransactionController {
             String mop = MOPCMB.getValue();
             String scheme = schemeCMB.getValue();
             String late = lateCMB.getValue();
-            String transacID = transacIDTF.getText();
             String amt = amtTF.getText();
 
             double balance = 0.0;  // Default balance value
@@ -283,12 +280,11 @@ public class TransactionController {
 	
 	private boolean validateInputs() {
 	    String mop = MOPCMB.getValue();
-	    String transacID = transacIDTF.getText();
 	    String late = lateCMB.getValue();
 	    String scheme = schemeCMB.getValue();
 	    String amt = amtTF.getText();
 
-	    if (mop == null || transacID.isEmpty() || late == null || scheme == null || amt.isEmpty()) {
+	    if (mop == null || late == null || scheme == null || amt.isEmpty()) {
 	        Alert alert = new Alert(AlertType.ERROR);
 	        alert.setTitle("Error");
 	        alert.setHeaderText("Invalid Inputs");
@@ -344,54 +340,63 @@ public class TransactionController {
 	}
 
 	private void saveAndPrint() {
-	    TransactionController trans = TransactionController.getInstance();
-	    String studCode1 = trans.getStudCode();
-	    String mop = MOPCMB.getValue();
-	    String transacID = transacIDTF.getText();
-	    String late = lateCMB.getValue();
-	    String scheme = schemeCMB.getValue();
+        TransactionController trans = TransactionController.getInstance();
+        String studCode1 = trans.getStudCode();
+        String mop = MOPCMB.getValue();
+        String late = lateCMB.getValue();
+        String scheme = schemeCMB.getValue();
 
-	    System.out.println("has Transaction scode " + studCode1);
+        if (mop == null || late == null || scheme == null) {
+            System.out.println("Please fill in all required fields.");
+            return;
+        }
 
-	    if (mop == null || transacID.isEmpty() || late == null || scheme == null) {
-	        System.out.println("Please fill in all required fields.");
-	        return;
-	    }
+        // Generate a 6-digit transaction ID
+        String transactID = generateRandomTransactionID();
 
-	    double totalAmount = calculateTotalAmount();
+        System.out.println("has Transaction scode " + studCode1);
 
-	    UserSession session = UserSession.getInstance();
-	    String encoder = session.getUsername();
+        double totalAmount = calculateTotalAmount();
 
-	    try (Connection con = DatabaseManager.getConnection()) {
-	        String sql = "UPDATE transaction SET payment_mode=?, amount=?, late=?, total=?, balance=?, misc_total=?, tuition_total=?, encoder=?, date=?, scheme=? WHERE scode=?";
+        UserSession session = UserSession.getInstance();
+        String encoder = session.getUsername();
 
-	        try (PreparedStatement preparedStatement = con.prepareStatement(sql)) {
-	            preparedStatement.setString(1, mop);
-	            preparedStatement.setString(2, amtTF.getText());
-	            preparedStatement.setString(3, late);
-	            preparedStatement.setString(4, totalLBL.getText());
-	            preparedStatement.setString(5, balanceLBL.getText());
-	            preparedStatement.setDouble(6, calculateMiscAmount());
-	            preparedStatement.setString(7, tuitionLBL.getText());
-	            preparedStatement.setString(8, encoder);
-	            preparedStatement.setString(9, getCurrentDate());
-	            preparedStatement.setString(10, scheme);
-	            preparedStatement.setString(11, studCode1);
+        try (Connection con = DatabaseManager.getConnection()) {
+            String sql = "UPDATE transaction SET transaction_id =?, payment_mode=?, amount=?, late=?, total=?, balance=?, misc_total=?, tuition_total=?, encoder=?, date=?, scheme=? WHERE scode=?";
 
-	            int rowsAffected = preparedStatement.executeUpdate();
+            try (PreparedStatement preparedStatement = con.prepareStatement(sql)) {
+                preparedStatement.setString(1, transactID);
+                preparedStatement.setString(2, mop);
+                preparedStatement.setString(3, amtTF.getText());
+                preparedStatement.setString(4, late);
+                preparedStatement.setString(5, totalLBL.getText());
+                preparedStatement.setString(6, balanceLBL.getText());
+                preparedStatement.setDouble(7, calculateMiscAmount());
+                preparedStatement.setString(8, tuitionLBL.getText());
+                preparedStatement.setString(9, encoder);
+                preparedStatement.setString(10, getCurrentDate());
+                preparedStatement.setString(11, scheme);
+                preparedStatement.setString(12, studCode1);
 
-	            if (rowsAffected > 0) {
-	                System.out.println("Transaction record updated successfully.");
-	            } else {
-	                System.out.println("No rows affected. Update failed.");
-	            }
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace(); // for development only, replace with proper logging
-	        System.out.println("An error occurred. Please contact support.");
-	    }
-	}
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    System.out.println("Transaction record updated successfully.");
+                } else {
+                    System.out.println("No rows affected. Update failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // for development only, replace with proper logging
+            System.out.println("An error occurred. Please contact support.");
+        }
+    }
+	
+	private String generateRandomTransactionID() {
+        Random random = new Random();
+        int transactionID = random.nextInt(900000) + 100000; // Generates a random number between 100000 and 999999
+        return String.valueOf(transactionID);
+    }
 
 
 	private String getCurrentDate() {
