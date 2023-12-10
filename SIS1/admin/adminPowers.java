acf9098e25555f9bc07d3fca5fa0ed1e91417ffb
Mark Acedo <gerceeacedo@gmail.com>
2023-12-10 10:01:22 +0800

    gawa ni krisha

---
+++ SIS1/admin/adminPowers.java
@@ -0,0 +1,23 @@
+package admin;
+
+public class adminPowers {
+
+	
+	public void addSection() {
+		//TODO insert new section to database isang character lang tapos 
+		//TODO 
+	}
+	
+	public void addCampus() {
+		//TODO insert Campus to Database
+	}
+	
+	public void addCourses() {
+		//TODO 
+	}
+	
+	public void addSubjects() {
+		//TODO
+	}
+	
+}
